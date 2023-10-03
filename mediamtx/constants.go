49f8087f048b6f8676f944601aadf7e45ba44977
Allan Nava <allannava95@gmail.com>
2023-10-03 10:30:03 +0200

    added healthcheck

---
+++ mediamtx/constants.go
@@ -0,0 +1,6 @@
+package mediamtx
+
+const (
+	GET_CONFIG = "/v2/config/get"
+	SET_CONFIG = "/v2/config/set"
+)
--- mediamtx/mediamtx.go
+++ mediamtx/mediamtx.go
@@ -10,3 +10,46 @@ type mediamtxSdk struct {
 	Url        string
 	restClient *resty.Client
 }
+
+
+type IMediamtxClient interface {
+	//
+	HealthCheck() error
+	//
+}
+
+
+func (o *mediamtxSdk) HealthCheck() error {
+	_, err := o.restyGet(o.Url, nil)
+	if err != nil {
+		return nil
+	}
+	return nil
+}
+
+
+// Resty Methods
+
+func (o *mediamtxSdk) restyPost(url string, body interface{}) (*resty.Response, error) {
+	resp, err := o.restClient.R().
+		SetHeader("Accept", "application/json").
+		SetBody(body).
+		Post(url)
+
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+// get request
+func (o *mediamtxSdk) restyGet(url string, queryParams map[string]string) (*resty.Response, error) {
+	resp, err := o.restClient.R().
+		SetQueryParams(queryParams).
+		Get(url)
+	//
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
