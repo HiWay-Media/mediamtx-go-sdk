105b98ce6e09265d03c09dbba8dca16ea911458f
Allan Nava <allannava95@gmail.com>
2023-10-03 10:43:44 +0200

    added new constants

--- mediamtx/constants.go
+++ mediamtx/constants.go
@@ -1,6 +1,11 @@
 package mediamtx
 
 const (
-	GET_CONFIG = "/v2/config/get"
-	SET_CONFIG = "/v2/config/set"
+	//
+	GET_CONFIG 			= "/v2/config/get"
+	SET_CONFIG 			= "/v2/config/set"
+	ADD_PATH_CONFIG 	= "/v2/config/paths/add/"
+	EDIT_PATH_CONFIG	= "/v2/config/paths/edit/"
+	REMOVE_PATH_CONFIG	= "/v2/config/paths/remove/"
+	//
 )
