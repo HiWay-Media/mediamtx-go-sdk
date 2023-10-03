8ea3e343dffb7b1da14002b92415aa2e3a99d102
Allan Nava <allannava95@gmail.com>
2023-10-03 10:51:21 +0200

    added doc

--- mediamtx/constants.go
+++ mediamtx/constants.go
@@ -1,7 +1,7 @@
 package mediamtx
 
 const (
-	//
+	// documentation https://bluenviron.github.io/mediamtx/
 	GET_CONFIG 			= "/v2/config/get"
 	SET_CONFIG 			= "/v2/config/set"
 	ADD_PATH_CONFIG 	= "/v2/config/paths/add/"
